3fd9a70bb665a829429ea3dc8118444650d405e9
clairepuleio <74558057+clairepuleio@users.noreply.github.com>
2021-04-05 12:10:24 -0700

    Update 1D1clast.03.m

--- 1D1clast.03.m
+++ 1D1clast.03.m
@@ -5,34 +5,32 @@
 clf;
 clear all;
 
+% Should only change dyclast, clastnum, tclasttwo and tclastthree
+
 clastnum = 1; % Number of clasts in spatter pile
 dyclast = 0.03; % (m) Thickness of one clast
 
-tnum = 30000; % Number of timesteps (How many timesteps do you want to run the code for?)
+tnum = 500000; % Number of timesteps (How many timesteps do you want to run the code for?)
 tclasttwo = 5.; % (s) Time when a new clast is deposited
-tclastthree = 10.; 
+tclastthree = 10.; % (s) Time when a third clast is deposited
 
 
 % Define model parameters
-% Naming scheme: x is a location. dx is a thickness. 
-% Node is nodal location (when combined with x) and number of nodes (when
-% combined with dx)
-% Should only change dxclast out of all model parameters 
+% Naming scheme: y is a location. dy is a thickness. 
+% Node is nodal location (when combined with y) and number of nodes (when
+% combined with dy) 
 dysystem = 4.; % (m) Thickness of entire system (area shown in model window)
 dysystemnode = (801); % Number of nodes for the thickness of the entire system. Controls resolution.
-dystep = dysystem/(dysystemnode-1); % (m) Length one grid step (dx)
+dystep = dysystem/(dysystemnode-1); % (m) Length one grid step (dy)
 dyclastnode = dyclast/dystep; % Number of nodes for thickness of one clast
 ypilebottom = 1.; % (m) Boundary between the ground and the bottom of the spatter pile
 ypilebottomnode = ((ypilebottom/dystep)+1); % Node of the boundary between the ground and the bottom of the spatter pile
 yclastonetop = (ypilebottom+dyclast); % (m) Boundary between the top of the clast and the air (if spatter pile has only one clast)
 yclastonetopnode = (ypilebottomnode+dyclastnode); % Node of the boundary between the top of the clast and the air (if spatter pile has only one clast)
-yclasttwotop = (yclastonetop + dyclast);
-yclasttwotopnode = (yclastonetopnode + dyclastnode); 
-yclastthreetop = (yclasttwotop + dyclast);
-yclastthreetopnode = (yclasttwotopnode + dyclastnode);
-%xpiletop = (xpilebottom+(dxclast)); % (m) Boundary betweem the top of the spatter pile and the air at the start
-%xpiletopnode = (xpilebottomnode+(dxclastnode)); % Node of the boundary between teh top of the spatter pile and the air at the start
-%xpiletopnode = 259;
+yclasttwotop = (yclastonetop + dyclast); % (m) Boundary between the top of the second clast and the air (if spatter pile has only two clasts)
+yclasttwotopnode = (yclastonetopnode + dyclastnode); % Node of the boundary between the top of the second clast and the air (if spatter pile has only two clasts)
+yclastthreetop = (yclasttwotop + dyclast); % (m) Boundary between the top of the third clast and the air (if spatter pile has only three clasts)
+yclastthreetopnode = (yclasttwotopnode + dyclastnode); % Node of the boundary between the top of the third clast and the air (if spatter pile has only three clasts)
 dypile = (dyclast*clastnum); % (m) Thickness of spatter pile
 dypilenode = (dyclastnode*clastnum); % Number of nodes for thickness of spatter pile
 jstep = 1; % step after every flow
@@ -61,10 +59,6 @@ sb = 0.000000056703; % W/m^2K^4 stefan-boltzmann constant
 dtexp = dystep^2/2/kappaclast; % (s) Limitation for explicit timestep 
 dt = dtexp./10; % (s) Length of one timestep
 
-% Things to vary with different model runs
-
-
-
 % Setting up the vector for nodal point positions (the righthand size of our matrix system is the vector) 
 y=0:dystep:dysystem; % The vector is essentially one grid step by the thickness of the system (0:0.005:2)
 
@@ -78,18 +72,16 @@ Tbackground = 293.; % (K) Initial temperature background for the entire system
 for i=1:1:dysystemnode % For all locations of i in the system
     T0imp(i) = Tbackground; % We say that initially the entire system is at 293K
 if(y(i)> ypilebottom && y(i)<= yclastonetop)
-    T0imp(i) = Tclast; % If we are located within the clast (including bottom node and top node of the clast) our initial temp is now 1403K
+    T0imp(i) = Tclast; % If we are located within the clast (including bottom node and top node of the clast) our initial temp is now 1373K
 end 
 end 
-
-
 
 % Time cycle
 timesum=0; % How much time has elapsed (we're still looking at what the model is initially, so no time has yet passed)
 
 % Now let's populate the matrix
-L = sparse(dysystemnode,dysystemnode); % The lefthand side of the matrix is 401 nodes by 401 nodes and is mostly made up of zeros (sparse)
-R = zeros(dysystemnode,1); % The righthand side of the matrix is the vector and it is 401 nodes by 1
+L = sparse(dysystemnode,dysystemnode); % The lefthand side of the matrix is 801 nodes by 801 nodes and is mostly made up of zeros (sparse)
+R = zeros(dysystemnode,1); % The righthand side of the matrix is the vector and it is 801 nodes by 1
 
 % Define the first point in the matrix and the first point in the vector
 L(1,1) = 1; % First point in the matrix is 1
@@ -126,7 +118,6 @@ for t=1:1:tnum
 % This gets us through one timestep. Then we set all the Tnew values that
 % we just solved for to Ti and we solve everything again to get new Tnew
 % values
-% So let's actually do this
 
 % First block matrix: Ground matrix
 % Tells model to calculated heat transfer within the ground
@@ -180,8 +171,6 @@ L(i,i) = 2*kappaair/(dystep^2) + 1/dt;
 L(i,i+1) = -kappaair/(dystep^2);
 R(i,1) = T0imp(i)/dt;
 end    
-
-
 
 % Define the last point in the matrix and the last point in the vector
 L(dysystemnode,dysystemnode) = 1;
@@ -263,8 +252,6 @@ if jstep == 3
     L(yclastthreetopnode,yclastthreetopnode) = ((-kappaclast)/(dystep^2)) + ((h)/(rhoclast*cpclast*dystep)) + (1/dt);
     R(yclastthreetopnode,1) = T0imp(i)/dt + ((Tair*h)/(rhoclast*cpclast*dystep)) + ((-emis*sb*(T0imp(i)^4))/(rhoclast*cpclast*dystep)) + ((emis*sb*(Tair^4))/(rhoclast*cpclast*dystep));
 
-    
-    
 % Third block matrix: Air Matrix
 % Tells model to calculate heat transfer within the air
 for i=yclastthreetopnode+1:1:dysystemnode-1
@@ -298,38 +285,6 @@ end
 % Let's actually put this into the matrix (remember that everything on
 % the LHS of the matrix is multipled by the Ti at that location)
 
-% Below is for radiation
-   % L(xpiletopnode,xpiletopnode-1) = (-kappaclast)/(dxstep^2);
-   % L(xpiletopnode,xpiletopnode) = ((kappaclast)/(dxstep^2)) + ((h)/(rhoclast*cpclast*dxstep)) + (1/dt);
-   % R(xpiletopnode,1) = Ti(i)/dt + ((293*h)/(rhoclast*cpclast*dxstep)) + ((-emis*sb*(Ti(i)^4))/(rhoclast*cpclast*dxstep)) + ((emis*sb*(293^4))/(rhoclast*cpclast*dxstep));
-%end
-
-% Below is for convection
-%L(xpiletopnode,xpiletopnode-1) = (-kappaclast)/(dxstep^2);
-%L(xpiletopnode,xpiletopnode) = ((kappaclast)/(dxstep^2)) + ((h)/(rhoclast*cpclast*dxstep)) + (1/dt);
-%R(xpiletopnode,1) = Ti(i)/dt + ((293*h)/(rhoclast*cpclast*dxstep));
-
-% Below is for conduction
-%L(xpiletopnode,xpiletopnode-1) = -kclast/(rhoclast*cpclast*(dxstep^2));
-%L(xpiletopnode,xpiletopnode) = (kclast/(rhoclast*cpclast*(dxstep^2))) + (kair/(rhoclast*cpclast*(dxstep^2))) + 1/dt;
-%L(xpiletopnode,xpiletopnode+1) = -kair/(rhoclast*cpclast*(dxstep^2));
-%R(xpiletopnode,1) = Ti(i)/dt;
-
-
-
-% Third block matrix: Air Matrix
-% Tells model to calculate heat transfer within the air
-%for i=xpiletopnode+1:1:dxsystemnode-1
-%L(i,i-1) = -kappaair/(dxstep^2);
-%L(i,i) = 2*kappaair/(dxstep^2) + 1/dt;
-%L(i,i+1) = -kappaair/(dxstep^2);
-%R(i,1) = Ti(i)/dt;
-%end    
-
-% Define the last point in the matrix and the last point in the vector
-%L(dxsystemnode,dxsystemnode) = 1;
-%R(dxsystemnode,1) = Tair;
-
 % Now let's actually solve this!
 % Obtaining solution for implicit temperature profile
 % We input values for the LHS and the RHS of the matrix. LHSx=RHS. Now we
@@ -353,71 +308,15 @@ if(timesum>(tclastthree) && timesum<(tclastthree+dt) && jstep<clastnum)
     jstep=jstep+1;
 end
     
-
-
-%if (timesum == tclastthree)
- %   Tnew(xclasttwotopnode+1:xclastthreetopnode)= Tclast;
-  %  jstep == 3;
-%end
-
-%if (timesum > tnewclast && timesum < tnewclast + dt && jstep < clastnum)
-%Tnew(xclastonetopnode+1:xclasttwotopnode)= Tclast; % added a new clast
-%tnew = tnewclast + tbetween; %update time for next clast
-%xpiletopnode = xpilebottomnode + (dxclastnode*jstep);
-%jstep = jstep + 1;
-%end
-
-% Let's plot this implicit solution so we can see how the spatter pile
-% cools with time
-% Open figure
-% Plotting implicit solution 
-%subplot(2,1,1);
-%plot(T0imp,y,'k');
-%axis([0.9*Tground 1.1*Tclast 0 dysystem]);
-%title(['Implicit: step=',num2str(t),' time,sec=',num2str(timesum)]);
-%ylabel('x, m');
-%xlabel('Temperature, K');
-% Plot cooling rate
-%subplot (2,1,2);
-%plot (timesum, T0imp(yclastonetopnode-1),'o'); %This is the "location" of the thermocouple, we want it to be one node below the top of the first clast
 timevec(b)=timesum;
 tempvec(b)=T0imp(yclastonetopnode-1);
 b=b+1;
 %hold on;
 disp(dt);
 disp(timesum);
-%disp(jstep);
-%disp(Ti(322));
-%disp(Ti(321));
-%disp(Ti(320));
-%disp(Ti(301));
-%disp(Ti(282));
-%disp(Ti(281));
-%disp(Ti(280));
-%disp(Ti(261));
-%disp(Ti(242));
-%disp(Ti(241));
-%disp(Ti(240));
-%disp(Ti(221));
-%disp(Ti(202));
-%disp(Ti(201));
 disp(T0imp(yclastonetopnode-1));
 disp(T0imp(yclastonetopnode));
 disp(T0imp(yclastonetopnode+1));
-%disp(T0imp(yclasttwotopnode+1));
-
-%axis([0 500 300 1410]);
-%xlabel('Time, sec');
-%ylabel('Temperature, K');
-%text (10,10,'1025 sec - 2nd flow');
-
-% Stop for 0.1 second
-%drawnow % draw above figure without delay
-%pause(0.1);
-
-% Stop for 0.1 second
-%drawnow % draw above figure without delay
-%pause(0.1);
 
 % Add time counter
 timesum = timesum+dt; 
@@ -427,9 +326,6 @@ timesum = timesum+dt;
 % say, let's make all the Tnew that we solved for be Ti, then we can solve
 % everything again and get new Tnew. 
 T0imp = T1imp;
-%if Ti(dxclastnode-1) < 890.
- %   break
-%end
 end
 
 b=1;
@@ -443,11 +339,5 @@ end
 
 format longg
 
-
-
-
 TimeFinal=Time(end)
 TempFinal=Temp(end)
-
-%A = jstep;
-%plot(A)
